6d6d2d897bac3e4aa48108e83b7f47b446daf99f
Mauricio Vargas <38122573+m4wnn@users.noreply.github.com>
2024-02-18 13:42:34 -0800

    jupyter notebook homework template

---
+++ jupyter-homework/main.py
@@ -0,0 +1,18 @@
+# %% [markdown]
+"""
+<script src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+"""
+# %% [markdown]
+"""
+<!--*!{misc/title.html}-->
+"""
+# %% [markdown]
+"""
+<!--*!{sections/q01.md}-->
+"""
+# %% [markdown]
+"""
+<!--*!{sections/q01-a.html}-->
+"""
+# %%
+print("Hello, World!")
